1406ccf26ac307c8c2724280e3f0133b106329ed
Gonzalo Merkel <merkelgonzalo.ok@gmail.com>
2023-04-17 17:42:33 -0300

    Initial structure

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import {Server} from 'socket.io';
+
+const PORT = '8080';
+
+const app = express();
+const httpServer = app.listen(PORT, ()=>{
+    console.log("Server running on port " + PORT);
+});
+const socketServer = new Server(httpServer);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+
+//NEW
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public')); //Important for use js y css files on templates
+app.use('/', viewsRouter)
